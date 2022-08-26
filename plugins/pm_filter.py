f1578eabdec6f8c19a25064827d971b42b3efe9a
Xvr35a <103354662+Xvr35a@users.noreply.github.com>
2022-08-26 18:43:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -734,7 +734,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        k = await msg.reply("I couldn't find any movie in that name.")
+        k = await msg.reply("I couldn't find any Scenepack in that name.")
         await asyncio.sleep(8)
         await k.delete()
         return
